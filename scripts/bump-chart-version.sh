fd258385f43d30f4433b145877646756ff028f96
Sebastian Gaiser <sebastiangaiser@users.noreply.github.com>
2024-08-18 19:09:26 +0200

    build(renovate): simplify Chart.yaml version bump

--- scripts/bump-chart-version.sh
+++ scripts/bump-chart-version.sh
@@ -1,39 +1,17 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
+#!/bin/bash
+set -eu
 parent_dir="$1"
-is_major="$2"
-is_minor="$3"
-is_patch="$4"
 
-version=$(grep "^version:" "charts/${parent_dir}/Chart.yaml" | awk '{print $2}')
-if [[ ! $version ]]; then
+ver=$(grep "^version:" "charts/${parent_dir}/Chart.yaml" | awk '{print $2}')
+
+if [ -z "$ver" ]; then
   echo "No valid version was found"
   exit 1
 fi
 
-major=$(echo "$version" | cut -d. -f1)
-minor=$(echo "$version" | cut -d. -f2)
-patch=$(echo "$version" | cut -d. -f3)
+# bump patch level of the version
+# will not work with rc and other suffixes
+bumped_ver="${ver%.*}.$(( ${ver##*.} + 1 ))"
 
-# Bump major version
-if [[ "$is_major" = 'true' ]]; then
-  major=$(( major + 1 ))
-  minor=0
-  patch=0
-fi
-
-# Bump minor version
-if [[ "$is_minor" = 'true' ]]; then
-  minor=$(( minor + 1 ))
-  patch=0
-fi
-
-# Bump patch version
-if [[ "$is_patch" = 'true' ]]; then
-  patch=$(( patch + 1 ))
-fi
-
-echo "Bumping version for $parent_dir from $version to $major.$minor.$patch"
-sed -i "s/^version:.*/version: ${major}.${minor}.${patch}/g" "charts/${parent_dir}/Chart.yaml"
+echo "Bumping version for $parent_dir from $ver to $bumped_ver"
+sed -i "s/^version:.*/version: ${bumped_ver}/g" "charts/${parent_dir}/Chart.yaml"
